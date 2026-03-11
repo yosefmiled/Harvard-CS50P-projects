0b1878ff626ebd91899c9ad8304fe0561f5e2e58
yosefmiled <yosefmiled1@gmail.com>
2026-03-11 23:54:31 +0300

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,21 @@
+
+
+math=input("Expression: ")
+x, y, z=math.split()
+if y== "+":
+    print(float(int(x)+int(z)))
+elif y== "*":
+    print(float(int(x)*int(z)))
+elif y== "-":
+    print(float(int(x)-int(z)))
+elif y== "/":
+    print(float(int(x)/int(z)))
+
+
+
+
+
+
+
+
+
