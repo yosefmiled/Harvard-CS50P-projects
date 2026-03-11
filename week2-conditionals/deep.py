9a2833cb67271a38337fe328bf9bace35c4ddf45
yosefmiled <yosefmiled1@gmail.com>
2026-03-12 00:16:06 +0300

    Delete week2-conditionals directory

--- week2-conditionals/deep.py
+++
@@ -1,6 +0,0 @@
-meaning= input("What is the Answer to the Great Question of Life, the Universe, and Everything? ").strip().lower()
-
-if meaning=="42" or meaning=="forty-two" or meaning=="forty two":
-    print("yes")
-else:
-    print("no")
